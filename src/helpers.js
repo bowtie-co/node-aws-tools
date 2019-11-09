f111dc328f654992708a6dbd4f646c3b844ccffe
Charlie McClung <charlie@bowtie.co>
2019-11-09 16:52:55 -0700

    Recursively prompt for input while invalid (and no default)

--- src/helpers.js
+++ src/helpers.js
@@ -16,6 +16,7 @@ const getArgFromCli = (cli, args, index, prompt, defaultValue = null, validateFn
     return val
   } else if (!defaultValue) {
     cli.warn(`Invalid arg:`, index, prompt)
+    return getArgFromCli(cli, args, index, prompt, defaultValue, validateFn)
   }
 
   return defaultValue
